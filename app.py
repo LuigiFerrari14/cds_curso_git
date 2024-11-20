477e610bf83c86e5fcf634ea9f328abf885ff27d
Luigi Ferrari <luigiferrar14@gmail.com>
2024-11-20 17:11:47 -0300

    Created extraction file and adjusted main file

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
-import pandas as pd
-import numpy as np
 import streamlit as st
-
-def load_data():
-    return pd.read_csv('data/processed/bikes_completed.csv')
+from src.extraction import load_data
+st.set_page_config(layout='wide')
 
 
 def main():
     df = load_data
 
+    st.dataframe(df)
 
 if __name__ == '__main__':
     main()
-
-
-
-
-df = load_data()
-
-st.dataframe(df)
