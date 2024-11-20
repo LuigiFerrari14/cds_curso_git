c33e57fac64ef956a6438b253e00ce9f004c6679
Luigi Ferrari <luigiferrar14@gmail.com>
2024-11-20 16:58:21 -0300

    Main function

--- app.py
+++ app.py
@@ -5,6 +5,17 @@ import streamlit as st
 def load_data():
     return pd.read_csv('data/processed/bikes_completed.csv')
 
+
+def main():
+    df = load_data
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
 df = load_data()
 
 st.dataframe(df)
